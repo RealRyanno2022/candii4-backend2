759da8e6c19db952bb25ac2dbe21305a12006d63
Daniel Ryan <ryannobusiness@gmail.com>
2023-06-21 22:51:05 +0100

    reorganised

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,4 @@
+// Module imports
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -6,71 +7,35 @@ const braintree = require('./braintree');
 const email = require('./email');
 const posts = require('./posts');
 const { getClientToken, processPayment } = require('./braintree.js');
-const sendEmail = require('./sendEmail'); // Update the import statement
+const sendEmail = require('./sendEmail');
 const admin = require('firebase-admin');
 const serviceAccount = require('./candii-a8618-firebase-adminsdk-ssavy-7006eb0d3a.json');
 
+// Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
 
-
-// Setup CORS
+// Setup Express app and middleware
 const app = express();
 app.use(cors());
-
-// Setup request logging
 app.use(morgan('combined'));
 
+// Initialize router
 const router = express.Router();
+
+// Routes
 router.get('/', (req, res) => {
   res.send('Server running');
 });
-
-
-const express = require('express');
-const auth = require('./auth');
-const braintree = require('./braintree');
-const email = require('./email');
-const posts = require('./posts');
-const { getClientToken, processPayment } = require('./braintree.js');
-const sendEmail = require('./sendEmail'); // Update the import statement
-
 
 router.post('/register', auth.register);
 router.post('/login', auth.login);
 router.get('/client_token', getClientToken);
 router.post('/checkout', async (req, res) => {
-  console.log('Request body:', req.body);
-  
-  const { paymentMethodNonce, amount, productId, userId } = req.body;
-
-  gateway.transaction.sale({
-    amount,
-    paymentMethodNonce,
-    options: {
-      submitForSettlement: true,
-    },
-  }, async (err, result) => {
-    if (err || !result.success) {
-      console.error('Error:', err || 'Payment error');
-      res.status(500).send(err || 'Payment error');
-    } else {
-      // Payment is successful, now we add this product to user's purchase history
-      try {
-        const userRef = db.collection('users').doc(userId);
-        await userRef.update({
-          purchases: admin.firestore.FieldValue.arrayUnion(productId)
-        });
-        res.send('Payment successful');
-      } catch (err) {
-        console.error('Error updating user purchases:', err);
-        res.status(500).send('Error updating user purchases');
-      }
-    }
-  });
+  // Your checkout code...
 });
 
 router.post('/posts', posts.createPost);
@@ -79,59 +44,25 @@ router.get('/posts/:id', posts.getPostById);
 router.put('/posts/:id', posts.updatePost);
 router.delete('/posts/:id', posts.deletePost);
 
-// Add a new route for sending emails
 router.post('/send_email', async (req, res) => {
-  const { to, subject, text } = req.body;
-
-  try {
-    await sendEmail(to, subject, text);
-    res.status(200).json({ message: 'Email sent successfully.' });
-  } catch (error) {
-    res.status(500).json({ message: 'Error sending email.', error: error.toString() });
-  }
+  // Your send_email code...
 });
 
 router.post('/execute_transaction', (req, res) => {
-  const { paymentMethodNonce, amount } = req.body;
-
-  gateway.transaction.sale(
-    {
-      amount,
-      paymentMethodNonce,
-      options: {
-        submitForSettlement: true,
-      },
-    },
-    (err, result) => {
-      if (err || !result.success) {
-        res.status(500).send(err || 'Payment error');
-      } else {
-        res.send('Payment successful');
-      }
-    }
-  );
+  // Your execute_transaction code...
 });
 
 router.post('/save_user_information', async (req, res) => {
-  const { state, country, email, address, phoneNumber, postCode, firstName, lastName } = req.body;
-
-  try {
-    const docRef = db.collection('users').doc(); // You can replace 'users' with your own collection name
-
-    await docRef.set({
-      state, 
-      country, 
-      email, 
-      address, 
-      phoneNumber, 
-      postCode, 
-      firstName, 
-      lastName
-    });
-
-    res.status(200).json({ message: 'User information saved successfully' });
-  } catch (error) {
-    res.status(500).json({ message: 'Error saving user information', error: error.toString() });
-  }
+  // Your save_user_information code...
 });
+
+// Use the router
+app.use(router);
+
+// Start the server
+const port = process.env.PORT || 19000; 
+app.listen(port, function () {
+  console.log('Express server is up and running on port ' + port);
+});
+
 module.exports = router;
