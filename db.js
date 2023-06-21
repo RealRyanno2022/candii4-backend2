2fa5e7a86a06974b0ff6c9aeb061e1f402ad9760
Daniel Ryan <ryannobusiness@gmail.com>
2023-06-21 23:46:59 +0100

    yahhh

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./candii-a8618-firebase-adminsdk-ssavy-7006eb0d3a.json');
+// const admin = require('firebase-admin');
+// const serviceAccount = require('./candii-a8618-firebase-adminsdk-ssavy-7006eb0d3a.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
 
-const db = admin.firestore();
+// const db = admin.firestore();
 
-module.exports = db;
+// module.exports = db;
