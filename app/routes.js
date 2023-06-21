ce0dc523f9344a43f4bd9c1e98cf5fe5e4019c3d
Daniel Ryan <ryannobusiness@gmail.com>
2023-06-21 22:57:16 +0100

    re org

--- app/routes.js
+++ app/routes.js
@@ -35,7 +35,34 @@ router.post('/register', auth.register);
 router.post('/login', auth.login);
 router.get('/client_token', getClientToken);
 router.post('/checkout', async (req, res) => {
-  // Your checkout code...
+  console.log('Request body:', req.body);
+  
+  const { paymentMethodNonce, amount, productId, userId } = req.body;
+
+  gateway.transaction.sale({
+    amount,
+    paymentMethodNonce,
+    options: {
+      submitForSettlement: true,
+    },
+  }, async (err, result) => {
+    if (err || !result.success) {
+      console.error('Error:', err || 'Payment error');
+      res.status(500).send(err || 'Payment error');
+    } else {
+      // Payment is successful, now we add this product to user's purchase history
+      try {
+        const userRef = db.collection('users').doc(userId);
+        await userRef.update({
+          purchases: admin.firestore.FieldValue.arrayUnion(productId)
+        });
+        res.send('Payment successful');
+      } catch (err) {
+        console.error('Error updating user purchases:', err);
+        res.status(500).send('Error updating user purchases');
+      }
+    }
+  });
 });
 
 router.post('/posts', posts.createPost);
@@ -45,15 +72,59 @@ router.put('/posts/:id', posts.updatePost);
 router.delete('/posts/:id', posts.deletePost);
 
 router.post('/send_email', async (req, res) => {
-  // Your send_email code...
+  const { to, subject, text } = req.body;
+
+  try {
+    await sendEmail(to, subject, text);
+    res.status(200).json({ message: 'Email sent successfully.' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error sending email.', error: error.toString() });
+  }
 });
 
 router.post('/execute_transaction', (req, res) => {
-  // Your execute_transaction code...
+  const { paymentMethodNonce, amount } = req.body;
+
+  gateway.transaction.sale(
+    {
+      amount,
+      paymentMethodNonce,
+      options: {
+        submitForSettlement: true,
+      },
+    },
+    (err, result) => {
+      if (err || !result.success) {
+        res.status(500).send(err || 'Payment error');
+      } else {
+        res.send('Payment successful');
+      }
+    }
+  );
 });
 
 router.post('/save_user_information', async (req, res) => {
-  // Your save_user_information code...
+  const { state, country, email, address, phoneNumber, postCode, firstName, lastName } = req.body;
+
+  try {
+    const docRef = db.collection('users').doc(); // You can replace 'users' with your own collection name
+
+    await docRef.set({
+      state, 
+      country, 
+      email, 
+      address, 
+      phoneNumber, 
+      postCode, 
+      firstName, 
+      lastName
+    });
+
+    res.status(200).json({ message: 'User information saved successfully' });
+  } catch (error) {
+    console.error('Failed to save user information:', error);
+    res.status(500).json({ message: 'Error saving user information', error: error.toString() });
+  }
 });
 
 // Use the router
