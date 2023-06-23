eb96b412e154d73d3d08190ac55583ad08e5661b
Daniel Ryan <ryannobusiness@gmail.com>
2023-06-23 22:25:42 +0100

    .

--- routes.js
+++ routes.js
@@ -47,10 +47,10 @@ const getClientToken = (req, res) => {
   });
 };
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/braintree-handler/public', 'index.html'));
 });
 
 const processPayment = (req, res) => {
